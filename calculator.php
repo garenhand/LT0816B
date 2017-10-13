802ab9506431dc518f24ed5a982e31603272a4f9
unknown <lgkidz1@gmail.com>
2017-10-13 11:18:13 +0700

    update

--- calculator.php
+++ calculator.php
@@ -18,54 +18,25 @@
 
 	public function days($m){
 		switch($m){
-			case 1:{
-			return 31;
-			break;
+			case 1:
+			case 3:
+			case 5:
+			case 7:
+			case 8:
+			case 10:
+			case 12:{
+				return 31;
+				break;
 			}
 			case 2:{
-			return 28;
-			break;
+				return 28;
+				break;
 			}
-			case 3:{
-			return 31;
-			break;
+			default:{
+				return 30;
+				break;
 			}
-			case 4:{
-			return 30;
-			break;
-			}
-			case 5:{
-			return 31;
-			break;
-			}
-			case 6:{
-			return 30;
-			break;
-			}
-			case 7:{
-			return 31;
-			break;
-			}
-			case 8:{
-			return 31;
-			break;
-			}
-			case 9:{
-			return 30;
-			break;
-			}
-			case 10:{
-			return 31;
-			break;
-			}
-			case 11:{
-			return 30;
-			break;
-			}
-			case 12:{
-			return 31;
-			break;
-			}	
+				
 		}
 	}
 ?>
